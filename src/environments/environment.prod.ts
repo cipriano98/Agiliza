aadab51c0f75657148f1c640c72943c02b487ed3
Cipriano98 <natancipriano98@gmail.com>
2021-06-24 21:20:10 -0300

    Adicionado vinculação com firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  api: 'http://www.poatransporte.com.br/php/facades/process.php?a=nc'
+  api: 'http://www.poatransporte.com.br/php/facades/process.php?a=nc',
+  accessTokenMapBox: 'pk.eyJ1IjoiY2lwcmlhbm85OCIsImEiOiJja3E3OHljdHowM3JtMzFudXhmcDF6M3J5In0.TH5MmHC3XdgOxB3YRALikg'
 };
